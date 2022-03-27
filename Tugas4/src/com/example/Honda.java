db2f40068d8d562d66abdcfd26defb1fb5056fa0
muhammadAufaTsaqief-Kelompok12 <aufa310802@gmail.com>
2022-03-27 16:07:34 +0700

    tugas

---
+++ Tugas4/src/com/example/Honda.java
@@ -0,0 +1,21 @@
+package com.example;
+import java.util.Scanner;
+public class Honda {
+    public void motot(Integer cash) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("---------------------------------");
+        System.out.println("Masukkan Budget anda (Dalam juta), ");
+        System.out.println("---------------------------------");
+        int Budget = scan.nextInt();
+        System.out.println("Budget anda" + Budget + "juta");
+
+if (Budget <= 15 ){
+    System.out.println("Budget anda cocok dengan Honda Beat");
+}
+else if (Budget >15 && Budget<=25){
+    System.out.println("Budget anda cocok dengan Honda Vario");}
+else if (Budget > 25){
+    System.out.println("Budget anda cocok dengan Honda PCX");
+}
+    }
+}
---
+++ Tugas4/src/com/example/Tugas.java
@@ -0,0 +1,95 @@
+package com.example;
+import java.util.Scanner ;
+public class Tugas {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("====================================");
+            System.out.println("Selamat Datang di Toko Sepeda Motor Yanti");
+            System.out.println("====================================");
+            System.out.println();
+            System.out.println("---------------------------------");
+            System.out.println("Silahkan pilih merk sepeda motor anda");
+            System.out.println("---------------------------------");
+            Scanner scan = new Scanner(System.in);
+            System.out.println("1. Suzuki");
+            System.out.println("2. Yamaha");
+            System.out.println("3. Honda");
+            int merk = scan.nextInt();
+            System.out.println();
+            switch(merk) {
+                case 1 :
+                    System.out.println("---------------------------------");
+                    System.out.println("Masukkan Budget anda (Dalam juta), ");
+                    System.out.println("---------------------------------");
+                    int Budget = scan.nextInt();
+                    System.out.println("Budget anda" + Budget+ "juta");
+                    if (Budget<=15 ){
+                        System.out.println("Budget anda cocok dengan Suzuki Nex II");
+
+                        System.exit(0);
+                    }
+                    else if (Budget > 15 && Budget <= 25){
+                        System.out.println("Budget anda cocok dengan Nex Crossover");
+                        System.exit(0);
+
+                    }
+                    else if (Budget > 25){
+                        System.out.println("Budget anda cocok dengan Suzuki GSX");
+                        System.exit(0);
+
+
+                    }
+
+
+                case 2 :
+                    System.out.println(kereta());
+                    System.exit(0);
+                    break;
+                case 3:
+                    Honda objek = new Honda();
+                    objek.motot(3);
+                    System.exit(0);
+                default:
+                    System.out.println("Pilihan belum tersedia");
+                    System.exit(0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+        }
+
+    }
+    private static Integer kereta(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("---------------------------------");
+        System.out.println("Masukkan Budget anda (Dalam juta), ");
+        System.out.println("---------------------------------");
+        int Budget = scan.nextInt();
+        System.out.println("Budget anda" + Budget + "juta");
+
+        if (Budget<=15){
+            System.out.println("Budget anda cocok dengan Yamaha Jupiter");
+        }
+        else if (Budget > 15 && Budget <= 25){
+            System.out.println("Budget anda cocok dengan Yamaha Freego");
+        }
+        else if (Budget > 25){
+            System.out.println("Budget anda cocok dengan Yamaha Nmax");
+        }
+return 0;
+    }
+}
